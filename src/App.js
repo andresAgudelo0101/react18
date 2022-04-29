3dd46a36de2619950f117f711f2e433a7d7038a4
andres <echeverry11298@gmail.com>
2022-04-29 14:57:19 -0500

    agregamos el estado del tema

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import MainPage from './pages/MainPage';
+import {createContext,useState} from "react";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+
+  const[theme,setTheme]=useState("light");
+
+  const changeTheme=()=>{
+    setTheme((curr)=>(curr==="light"?"dark":"light"));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>tu no mete cabra samamabrique</h1>
-      </header>
+  <ThemeContext.Provider value={{theme,changeTheme}}>
+    <div className="App" id="light">
+      <MainPage/>
     </div>
+  </ThemeContext.Provider>
   );
 }
 
